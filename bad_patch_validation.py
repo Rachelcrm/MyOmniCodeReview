2ed38b6d79ad75717e2edd28c7e96cd11c0835b2
Carter Larsen <cjl259@cornell.edu>
2025-07-20 12:19:58 -0400

    Add Agentless support for Java

--- bad_patch_validation.py
+++ bad_patch_validation.py
@@ -7,18 +7,36 @@ import sys
 parser = argparse.ArgumentParser(description="Verify Bad Patch")
 parser.add_argument( "--instance_id", help="Instance ID")
 parser.add_argument("--results_folder", help="run evaluation folder name (after logs/run_evaluation/)")
-parser.add_argument("--dataset_name", default="data/codearena_instances.json",
+# parser.add_argument("--dataset_name", default="data/codearena_instances.json",
+#                     help="Name of the dataset")
+# parser.add_argument("--dataset_name", default="data/agentless_instances.json",
+#                     help="Name of the dataset")
+parser.add_argument("--dataset_name", default="data/java_instances.json",
                     help="Name of the dataset")
+parser.add_argument("--language", default="python")
 args = parser.parse_args()
 
-results_dir = os.path.join('logs/run_evaluation', args.results_folder)
+if args.language == 'python':
+    results_dir = os.path.join('logs/run_evaluation', args.results_folder)
+elif args.language == 'java':
+    results_dir = os.path.join('multiswebench_runs/BugFixing')
 
 if not os.path.exists(results_dir):
     print('Results folder does not exist (likely due to patch being empty)', results_dir)
     sys.exit(1)
 
-full_dir = os.path.join(results_dir, 'agentless', args.instance_id)
-report_path = os.path.join(full_dir, 'report.json')
+if args.language == 'python':
+    full_dir = os.path.join(results_dir, 'agentless', args.instance_id)
+    work_dir = full_dir
+    report_path = os.path.join(full_dir, 'report.json')
+elif args.language == 'java':
+    full_dir = os.path.join(results_dir, 'output', f"run_{args.results_folder}")
+    work_dir_temp = os.path.join(results_dir, 'workdir', f"run_{args.results_folder}")
+    for root, dirs, files in os.walk(work_dir_temp):
+        if 'fix.patch' in files:
+            work_dir = root
+            break
+    report_path = os.path.join(full_dir, 'final_report.json')
 
 if not os.path.exists(report_path):
     print('Report file does not exist (likely due to error in building image)', report_path)
@@ -27,9 +45,12 @@ if not os.path.exists(report_path):
 with open(report_path, 'r') as f:
     report = json.load(f)
 
-resolved = report[args.instance_id]['resolved']
-if resolved:
-    print('Solved task:', results_dir)
+if args.language == 'python':
+    unresolved = report[args.instance_id]['unresolved']
+elif args.language == 'java':
+    unresolved = report['unresolved_instances'] > 0
+if not unresolved:
+    print('Solved task or Error:', results_dir)
     sys.exit(1)
 else:
 
@@ -37,24 +58,36 @@ else:
         dataset = json.load(f)
 
     # load the patch from predictions path
-    patch_path = os.path.join(full_dir, 'patch.diff')
+    if args.language == 'python':
+        patch_path = os.path.join(work_dir, 'patch.diff')
+    elif args.language == 'java':
+        patch_path = os.path.join(work_dir, 'fix.patch')
     with open(patch_path, 'r') as f:
         patch = f.read()
 
     # add patch to dataset
-    task_ix = [i for i in range(len(dataset)) if dataset[i]['instance_id'] == args.instance_id][0]
-    # if 'bad_patches' in dataset[task_ix]:
-    #     dataset[task_ix]['bad_patches'].append(patch)
-    # else:
-    #     dataset[task_ix]['bad_patches'] = [patch]
-    dataset[task_ix]['bad_patch'] = patch
+    try:
+        task_ix = [i for i in range(len(dataset)) if dataset[i]['instance_id'] == args.instance_id][0]
+    except:
+        print("!!! instance not in output file already")
+    
+    if 'bad_patches' in dataset[task_ix]:
+        index = len(dataset[task_ix]['bad_patches']) + 1
+        dataset[task_ix]['bad_patches'].append({
+            "idx": index,
+            "source": "agentless",
+            "patch": patch
+            })
+    else:
+        dataset[task_ix]['bad_patches'] = [patch]
+    # dataset[task_ix]['bad_patch'] = patch
 
     # save dataset back to json file
     with open(args.dataset_name, 'w') as f:
         json.dump(dataset, f, indent=4)
 
     # save gold patch to gold.diff for easy comparison
-    gold_path = os.path.join(full_dir, 'gold.diff')
+    gold_path = os.path.join(work_dir, 'gold.diff')
     with open(gold_path, 'w+') as f:
         f.write(dataset[task_ix]['patch'])
 
